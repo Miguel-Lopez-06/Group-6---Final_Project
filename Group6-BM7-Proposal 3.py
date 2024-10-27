046cbe89747a4c49dfb3fa33cbd58e2e97ecf6d9
Miguel-Lopez-06 <vxleugim@gmail.com>
2024-10-27 18:47:32 +0800

    update

---
+++ Group6-BM7-Proposal 3.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import altair as alt
+import squarify
+from wordcloud import WordCloud
+from mpl_toolkits.mplot3d import Axes3D
+import networkx as nx
+import plotly.graph_objects as go
+from io import StringIO
+
+st.title('Data Visualization Using Streamlit')
+st.markdown('`Group 6 BM7`')
+st.header('------------------------------------------------------------')
