7ff5af207f53a83517abda66b2b4737fcbc36bd5
Miguel-Lopez-06 <vxleugim@gmail.com>
2024-10-27 22:46:44 +0800

    update

--- Group6-BM7-Proposal_3.py
+++ Group6-BM7-Proposal_3.py
@@ -2,7 +2,6 @@ import streamlit as st
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
 import seaborn as sns
