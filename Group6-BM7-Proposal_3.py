7b2f239c10b90692570806cf3af12c33cd3d9497
Miguel-Lopez-06 <vxleugim@gmail.com>
2024-10-31 23:59:42 +0800

    update

--- Group6-BM7-Proposal_3.py
+++ Group6-BM7-Proposal_3.py
@@ -114,3 +114,9 @@ plt.title("ROC Curve for Decision Tree")
 plt.legend(loc="lower right")
 st.pyplot(plt)
 plt.clf()
+
+st.header('Conclusion')
+st.subheader('**1. Price Distribution:**')
+st.write('*   Laptops typically cost around €1134.97.')
+st.write('*   Range of prices: At least: €174, Maximum: €6099, '
+          '25% of the total: €609, and 75% of the total: €1496.50')
