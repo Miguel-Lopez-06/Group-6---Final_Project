7b2d49ea19b0dda6b6c5e1c06f0938c16d16ee6d
Miguel-Lopez-06 <vxleugim@gmail.com>
2024-10-31 23:15:09 +0800

    update

--- Group6-BM7-Proposal_3.py
+++ Group6-BM7-Proposal_3.py
@@ -3,7 +3,6 @@ from io import StringIO
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, confusion_matrix, roc_curve, auc
