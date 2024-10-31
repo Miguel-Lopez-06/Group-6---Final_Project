7ff4c3aa61eb2c535f4713b0b7b12d4cb84f8c6c
Miguel-Lopez-06 <vxleugim@gmail.com>
2024-10-31 23:16:21 +0800

    update

--- Group6-BM7-Proposal_3.py
+++ Group6-BM7-Proposal_3.py
@@ -1,13 +1,7 @@
 import streamlit as st
-from io import StringIO
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score, confusion_matrix, roc_curve, auc
-from sklearn import tree
-from sklearn.preprocessing import LabelEncoder
 import seaborn as sns
 import altair as alt
 import squarify
@@ -15,7 +9,7 @@ from wordcloud import WordCloud
 from mpl_toolkits.mplot3d import Axes3D
 import networkx as nx
 import plotly.graph_objects as go
-
+from io import StringIO
 
 st.title('Group 6 BM7 - Proposal #3')
 st.markdown('`Diabetes Dataset`')
